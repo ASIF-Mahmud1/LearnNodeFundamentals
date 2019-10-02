2e12810d643461d6ea7203db010d4d8098fed48a
Asif Mahmud <asif01050105@gmail.com>
2019-10-02 17:00:19 +0600

    garbage collection on chrome dev tools

---
+++ garbageCollection.js
@@ -0,0 +1,4 @@
+// How javascript works: https://blog.sessionstack.com/how-does-javascript-actually-work-part-1-b0bacc073cf
+// gitHu link: https://github.com/workshopper/scope-chains-closures/tree/master/exercises/garbage-collection
+// Garbage collection is the decision process taken by the run-time for allocating and freeing memories
+// run time uses the algorithm:  Mark and Sweep
