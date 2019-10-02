02b01cade0b250cf0e0d7daa5f9bb4ae9a4e654c
Asif Mahmud <asif01050105@gmail.com>
2019-10-02 15:20:23 +0600

    Simple example of closure

--- closure.js
+++ closure.js
@@ -1,7 +1,10 @@
 // https://www.c-sharpcorner.com/UploadFile/f50501/working-with-closure-in-nodejs/
-function foo(){
-    var bar
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-closure-b2f0d2152b36
 
+// **** Testing with Closure   ****///
+function foo(number){
+    var bar
+    console.log('The number is ',number)
     function zip()
     {
         bar= true
@@ -9,7 +12,28 @@ function foo(){
     }
     return zip
 }
+// foo(5)
+//  let returnFromFoo= foo()
+//  returnFromFoo()
 
-let returnFromFoo= foo()
+// **** Testing with Closure  ****///
 
-returnFromFoo()
+/// *** Simple example that uses closure  *** ////
+
+function substractTwoNumbers(firsNum, secondNum)
+{
+    let result= firsNum-secondNum
+    console.log("The difference is ",result )
+
+    function multiplyResultByUserInput(userInput)
+    {
+        return  result*userInput
+    }
+    return multiplyResultByUserInput
+}
+
+ let multiplyBy= substractTwoNumbers(10,2)
+
+ console.log(multiplyBy(5))
+
+/// *** Simple example that uses closure  *** ////
