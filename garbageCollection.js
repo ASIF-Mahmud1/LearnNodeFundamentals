3491159ae2509b6912cca6ce05016f76122d0c11
Asif Mahmud <asif01050105@gmail.com>
2019-10-03 11:08:25 +0600

    package installed for promise

--- garbageCollection.js
+++
@@ -1,4 +0,0 @@
-// How javascript works: https://blog.sessionstack.com/how-does-javascript-actually-work-part-1-b0bacc073cf
-// gitHu link: https://github.com/workshopper/scope-chains-closures/tree/master/exercises/garbage-collection
-// Garbage collection is the decision process taken by the run-time for allocating and freeing memories
-// run time uses the algorithm:  Mark and Sweep
