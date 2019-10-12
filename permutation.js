6cd6fdba90b4f1abaef1c97bc7c40fb82af2a803
Asif Mahmud <asif01050105@gmail.com>
2019-10-12 22:58:38 +0600

    feat: Permutation :)

--- permutation.js
+++ permutation.js
@@ -1,24 +1,18 @@
-let outer= ['a','b','c','d']
-let inner= ['a','b','c','d']
-
-outer.forEach(function(node, index, theArray) {
-    inner.forEach((item,index2)=>{
-        if( ! node.includes(item) )
-        {
-            outer.push(node+item)
-        }
-    })
-    console.log(outer.length, outer)
+let outer= ['a','b','c','d','e']
+let inner= ['a','b','c','d','e']
+let counter=0
+while(counter<4)
+{
+    outer.forEach(function(node, index, theArray) {
+        inner.forEach((item,index2)=>{
+            if( ! node.includes(item) )
+            {
+                outer.push(node+item)
+            }
+        })
     
-  //  theArray[index].visited =false
-  })
-
-  console.table(outer)
-
-
-//////////////////////////
-//   let str= 'asadsa'
-
-//   let a= 'b'
-//   let p= str.includes(a)
-// console.log(p)
+      })
+    counter++
+}
+let unique = [...new Set(outer)];
+console.table(unique)
