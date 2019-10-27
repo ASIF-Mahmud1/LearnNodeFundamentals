7bf0a867f1bc47266db5f2c935eb979544437892
Asif Mahmud <asif01050105@gmail.com>
2019-10-27 21:43:30 +0600

    feat: on clicking submit button returns if the word exist in the array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,29 +3,47 @@ import './App.css';
 class  App extends React.Component {
   state= {
     typeWord: '',
-    wordList:[]
+    wordList:[],
+    wordFound: ''
   }
   componentDidMount= ()=>{
 
     fetch("http://localhost:4000/listWords")
     .then(response => response.json())
     .then(responseJson => {
+      console.log(responseJson)
       this.setState({wordList:responseJson})
     })
 
   }
   handleChange=(event)=>{
+    //this.state.wordList.indexOf(event.target.value)
     this.setState({
       typeWord:event.target.value
     }) 
   }
   submitWord=()=>{
+    //alert(this.state.typeWord)
+    
+    let index=this.state.wordList.findIndex((word)=>{
+      return word=== this.state.typeWord
+    }) 
 
+    if(  index>=0 )
+    {
+    // alert(`Word found at  index ${index}`)
+     this.setState({wordFound:`Word found at  index ${index}`})
+    }
+    else
+    {
+     this.setState({wordFound:"Not Found" })
+
+    }
      
   }
 render()
 {
- this.state.wordList.length>0 ? console.log(this.state.wordList): console.log("wordList Empty")
+ //this.state.wordList.length>0 ? console.log(this.state.wordList): console.log("wordList Empty")
   return (
     <div className="App">
       <header className="App-header">
@@ -34,6 +52,10 @@ render()
           <input style={{height: 40, fontSize:20,textAlign:"center"}} placeholder="please enter the word" type="text" value={this.state.typeWord} onChange={(e)=>{this.handleChange(e)}} />
         </label>
         <button style={{marginTop:50,height:50,width: 100, fontSize:20,textAlign:"center"}} onClick ={()=>{this.submitWord()}} > Submit</button>
+        {
+          this.state.wordFound !== '' &&
+          <label>{this.state.wordFound} </label>
+        }
       </header>
     </div>
   );
