9725d8a684f93ec6ef406485cdb0a224c674a5b4
Asif Mahmud <asif01050105@gmail.com>
2019-11-07 15:10:06 +0600

    files deleted

--- warmup.js
+++
@@ -1,9 +0,0 @@
-/* What is promise? 
-A promise is an object that defines a method called
-then. The promise object represents a value that may be available some time
-*/
-function printMessageAfterSomeTime(){   /// TIMED out will be shown after 0.3 sec
-    setTimeout(function(){ console.log("TIMED OUT!"); }, 300);
-
-}
-printMessageAfterSomeTime()
