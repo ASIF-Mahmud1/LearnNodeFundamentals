82f42b8603734448668d4d741d6240042ffc637b
Asif Mahmud <asif01050105@gmail.com>
2019-12-27 20:09:37 +0600

    first program mathjax

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,96 +1,44 @@
- import React from 'react';
-// import './App.css';
-import MathJax from 'react-mathjax2'
-// const ascii = 'U = 1/(R_(si) + sum_(i=1)^n(s_n/lambda_n) + R_(se))'
+import React, { Component } from 'react'
 
-// console.log(MathJax)
-// class  App extends React.Component {
-//   state= {
-//     typeWord: '',
-//     wordList:[],
-//     wordFound: ''
-//   }
-//   componentDidMount= ()=>{
+import MathJax from 'react-mathjax-preview'
 
-//     fetch("http://localhost:4000/listWords")
-//     .then(response => response.json())
-//     .then(responseJson => {
-//       //console.log(responseJson)
-//       this.setState({wordList:responseJson.words})
-//     })
+const asciimath = '`sum_(i=1)^n i^3=((n(n+1))/2)^2`'
+const math = String.raw`
+  <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+    <menclose notation="circle box">
+      <mi> x </mi><mo> + </mo><mi> y </mi>
+    </menclose>
+  </math>
 
-//   }
-//   handleChange=(event)=>{
-//     //this.state.wordList.indexOf(event.target.value)
-//     this.setState({
-//       typeWord:event.target.value
-//     },()=>{this.checkIfWordFound() }) 
-//   }
-//   checkIfWordFound=()=>{
-//     //alert(this.state.typeWord)
-    
-//     let index=this.state.wordList.findIndex((word)=>{
-//       return word=== this.state.typeWord
-//     }) 
+  $\lim_{x \to \infty} \exp(-x) = 0$
 
-//     if(  index>=0 )
-//     {
-//     // alert(`Word found at  index ${index}`)
-//      this.setState({wordFound:`${this.state.typeWord} found at  index ${index}`})
-//     }
-//     else
-//     {
-//      this.setState({wordFound:"Not Found" })
-
-//     }
-     
-//   }
-// render()
-// {
-//  //this.state.wordList.length>0 ? console.log(this.state.wordList): console.log("wordList Empty")
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//       <label >
-//           Word:
-//           <input style={{height: 40, fontSize:20,textAlign:"center"}} placeholder="please enter the word" type="text" value={this.state.typeWord} onChange={(e)=>{this.handleChange(e)}} />
-//         </label>
-//         {/* <button style={{marginTop:50,height:50,width: 100, fontSize:20,textAlign:"center"}} onClick ={()=>{alert("Button works")}} > Submit</button> */}
-//         {
-//           this.state.wordFound !== '' &&
-//           <label>{this.state.wordFound} </label>
-//         }
-//       </header>
-//       <div>
-//             <MathJax.Context input='ascii'>
-//                 <div>
-//                     <MathJax.Node>{ascii}</MathJax.Node>
-//                 </div>
-//             </MathJax.Context>
-//         </div>
-//     </div>
-//   );
-// }
-// }
-
-// export default App;
-
-// import MathJax from 'react-mathjax2'
+   ${asciimath}`
 
 const tex = `f(x) = \\int_{-\\infty}^\\infty\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,d\\xi`
+const ascii = 'U = 1/(R_(si) + sum_(i=1)^n(s_n/lambda_n) + R_(se))'
 
-export default class App extends React.Component {
-  render(){
+export default class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      math
+    }
+  }
+  handleChange = (event) => {
+    this.setState({
+      math: event.target.value
+    })
+  }
+  render() {
     return (
-        <div>
-          <h3>fsdfds</h3>
-            {/* <MathJax.Context input='tex'>
-                <div>
-                    <MathJax.Node>{tex}</MathJax.Node>
-                </div>
-            </MathJax.Context> */}
-        </div>
-    );
+
+      <>
+        <label>
+          <textarea style={{ height: 100, width: 600 }} type="text" value={this.state.math} onChange={(event) => this.handleChange(event)} />
+
+        </label>
+        <MathJax math={this.state.math} />
+      </>
+    )
   }
 }
-
