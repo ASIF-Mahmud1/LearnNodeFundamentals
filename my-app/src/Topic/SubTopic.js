e84e0b8caa7de0a2bd686160b1fb8ac879588d8e
Asif Mahmud <asif01050105@gmail.com>
2020-01-02 00:58:42 +0600

    component skeleton created: Topic, SubTopic

--- my-app/src/Topic/SubTopic.js
+++ my-app/src/Topic/SubTopic.js
@@ -6,20 +6,19 @@ export default class SubTopic extends React.Component {
 
         }
     }
+    
 
     render() {
         return (
             <>
-             <h3>
-                     {this.props.SubTopicTitle}
+             <h2>
+                  SubTopic:   {this.props.subTopicTitle? this.props.subTopicTitle: "Add SubTopic" }
+                </h2>
+                <h3>OverView: { this.props.overView? " "+this.props.overView: "Add OverView"}
+                    
                 </h3>
-                <h3>OverView:
-                     <h4>
-                        {this.props.overView}
-                    </h4>
-                </h3>
-        <h3>{this.props.body}</h3>
-        <h3>Summary :{this.props.summary}</h3>
+        <h3>{this.props.body ?this.props.body: "Add Content!"}</h3>
+        <h3>Summary :{this.props.summary ?this.props.summary:"Add Summary" }</h3>
 
             </>
         )
--- my-app/src/Topic/Topic.js
+++ my-app/src/Topic/Topic.js
@@ -1,27 +1,38 @@
 import React from 'react'
 import SubTopic from './SubTopic'
-const subTopic= {
+const subTopic = {
     subTopicTitle: "Distance between two points",
     overView: "We will figuire out how to find distance between two points using a formula",
     body: "This topic is explained using diagram",
-    summary : "That's all for today :) !"
+    summary: "That's all for today :) !"
 
 }
-export default class Topic extends React.Component{
-constructor(props){
-    super(props)
-    this.state={
-
+export default class Topic extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            subTopic: []
+        }
     }
-}
+    addSubTopic = () => {
+        let addSubTopic = this.state.subTopic
+        addSubTopic.push(<SubTopic  />)
+        this.setState({ subTopic: addSubTopic }, () => { console.log(this.state.subTopic) })
+    }
 
-render()
-{
-    return(
-        <>
-        <h2>Topology</h2>
-        <SubTopic subTopicTitle={subTopic.subTopicTitle} overView={subTopic.overView} body={subTopic.body} summary={subTopic.summary}/>
-        </>
-    )
-}
+    render() {
+        return (
+            <>
+                <h2>Topology</h2>
+                <button onClick={() => { this.addSubTopic() }}>Add SubTopic</button>
+                <SubTopic subTopicTitle={subTopic.subTopicTitle} overView={subTopic.overView} body={subTopic.body} summary={subTopic.summary} />
+
+                {this.state.subTopic.map((item, index) => (
+                    < div key= {index}>
+                    {item}
+                    </div>
+                ))}
+            </>
+        )
+    }
 }
