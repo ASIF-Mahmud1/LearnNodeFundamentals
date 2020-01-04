6c3a432c4ab207c8908789b564c7166ac2138544
Asif Mahmud <asif01050105@gmail.com>
2020-01-04 23:27:18 +0600

    more features of tiny mce, cleaner UI on TinyMce.js

--- my-app/src/Math/TinyMce.js
+++ my-app/src/Math/TinyMce.js
@@ -70,10 +70,24 @@ class MyComponent extends React.Component {
     render() {
         return (
             <>
-                <Editor value={this.state.content} onEditorChange={this.handleEditorChange} />
+                <Editor value={this.state.content} onEditorChange={this.handleEditorChange} 
+                 init={{
+                    height: '400',
+                    fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+                    //  width: "800",
+                    plugins:
+                      'link image code table lists advlist wordcount anchor autolink codesample  textcolor',
+                    advlist_number_styles:
+                      'default lower-alpha lower-greek lower-roman upper-alpha upper-roman',
+                    toolbar:
+                      'undo redo | bold italic | alignleft aligncenter alignright | code | bullist numlist outdent indent wordcount fontsizeselect | anchor | codesample | fontselect | forecolor backcolor',
+                    font_formats:
+                      'Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva;    Webdings=webdings; Wingdings=wingdings,zapf dingbats',
+                  }}
+                />
 
-                <div dangerouslySetInnerHTML={{ __html: this.state.content }} />
-        <div>{ " $ f(x) = x^2$ is an example"}</div>
+                {/* <div dangerouslySetInnerHTML={{ __html: this.state.content }} />
+        <div>{ " $ f(x) = x^2$ is an example"}</div> */}
 
         <label>  <br /><b> Output :</b>     <MathJax math={this.state.math} >  {" $ f(x) = x^2$ is an example"} </MathJax> </label>
         <textarea style={{ height: 100, width: 600 }} type="text" value={this.state.math} onChange={(event) => this.handleChange(event)} />
